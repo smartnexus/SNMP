60fde7b0c9209cbfff203e92b9300cf8d6a0ed88
jacjurtab <rojacin99@gmail.com>
2020-04-24 13:09:29 +0200

    Añadido test_slack_serv.py y token del bot

--- slack_api.py
+++ slack_api.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, make_response, Response
-#from slackclient import SlackClient
+from slackclient import SlackClient
 
 #
 # Bot functions to send and receive messages from Slack Chat.
 #
 
 # TODO: Generate Slack chat to start config environment.
-SLACK_BOT_TOKEN = ""
+SLACK_BOT_TOKEN = "xoxb-1096471659041-1069373940215-BrYrc73i9UcbV5X7gaOBjh3v"
 SLACK_VERIFICATION_TOKEN = ""
 
 # Slack client for Web API requests
-#slack_client = SlackClient(SLACK_BOT_TOKEN)
+slack_client = SlackClient(SLACK_BOT_TOKEN)
 
 # Flask web server for incoming traffic from Slack
 app = Flask(__name__)
---
+++ test_slack_serv.py
@@ -0,0 +1,5 @@
+import pytest
+from slack_api import *
+
+def test_slackConnect():
+    print slackConnect()
