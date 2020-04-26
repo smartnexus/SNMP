5a4fe770e1cd8ed587617ea3cc0504fa0168b368
smartnexus <angrodboh@alum.us.es>
2020-04-26 20:09:54 +0200

    Comenzando con los primeros mensajes interactivos en slack

--- slack_api.py
+++ slack_api.py
@@ -1,7 +1,4 @@
-import os
-import time
 from slack import WebClient
-from slack.errors import SlackApiError
 from flask import Flask, request, make_response, Response
 import json
 
@@ -9,12 +6,14 @@ app = Flask(__name__)
 
 slack_token = "xoxb-1096471659041-1086356946467-hgDY5l937lAbsnZPmO8C8hm7"
 verification_token = "OWsReC2914G1XxtLbhSNYjly"
+channel_gestores = 'C012PN83MQC'
+channel_usuarios = 'C012AR4E5T8'
 client = WebClient(token=slack_token)
 
 
-def send_slack_message(ch):
+def send_init():  # Sends message to admins inviting them to start the test.
     return client.chat_postMessage(
-        channel=ch,
+        channel=channel_gestores,
         as_user=True,
         blocks=[
             {
@@ -37,7 +36,7 @@ def send_slack_message(ch):
                             "text": "Comenzar la prueba :heavy_check_mark:",
                             "emoji": True
                         },
-                        "value": "click_me_123"
+                        "value": "start_test"
                     }
                 ]
             }
@@ -45,32 +44,114 @@ def send_slack_message(ch):
     )
 
 
+def send_state_waiting(ts):  # Updates message to admins telling them to app is waiting users.
+    return client.chat_update(
+        channel=channel_gestores,
+        as_user=True,
+        ts=ts,
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Bienvenido al sistema de comprobación de rendimiento para diferentes aplicaciones de software mediante consultas vía SNMP.\n \n Se le recuerda que es necesario *disponer de un agente SNMP* de windows instalado en *todos los equipos* donde se vaya a realizar la prueba y que dichos usuarios *estén dentro* del canal #usuarios.\n\n Cuando desee comenzar la prueba *pulse el botón comenzar*.\n\n"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":warning:*AVISO*:  La prueba se esta desarrollando."
+                }
+            }
+        ]
+    )
+
+
+def send_invite_users():
+    return client.chat_postMessage(
+        channel=channel_usuarios,
+        as_user=True,
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":warning:*AVISO* Un gestor ha comenzado recientemente una prueba. Recuerda que para poder participar tendras que *iniciar previamente* el software y obtener tu *dirección ip*. ¡Haz click en el botón para participar!"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Participar en la prueba :bar_chart:",
+                            "emoji": True
+                        },
+                        "value": "subscribe_test"
+                    }
+                ]
+            }
+        ]
+    )
+
+
+def send_state_subscribed(ts):
+    return client.chat_update(
+        channel=channel_usuarios,
+        as_user=True,
+        ts=ts,
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":warning:*AVISO* Un gestor ha comenzado recientemente una prueba. Recuerda que para poder participar tendras que *iniciar previamente* el software y obtener tu *dirección ip*. ¡Haz click en el botón para participar!"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":heavy_check_mark: Tu subscripción se ha registrado correctamente."
+                }
+            }
+        ]
+    )
+
+
 @app.route("/slack/actions", methods=["POST"])
 def message_actions():
-    # Parse the request payload
     form_json = json.loads(request.form["payload"])
 
-    # Verify that the request came from Slack
-    print(form_json["token"])
-    print(form_json)
-
-    # Check to see what the user's selection was and update the message accordingly
-    # selection = form_json["actions"][0]["selected_options"][0]["value"]
-
-    # if selection == "cappuccino":
-    # message_text = "cappuccino"
-    # else:
-    # message_text = "latte"
-
-    # response = client.chat_update(
-    #   channel=form_json["channel"]["id"],
-    #   ts=form_json["message_ts"],
-    #   text="One {}, right coming up! :coffee:".format(message_text),
-    # )
-
-    # Send an HTTP 200 response with empty body so Slack knows we're done here
+    if form_json["token"] == verification_token:
+        value = form_json["actions"][0]["value"]
+        if value == "start_test":  # An admin has started the test.
+            admin = form_json['user']['username']
+            print('[Slack API] ' + admin + ' has started the test.')
+            ts = form_json['container']['message_ts']
+            send_state_waiting(ts)
+            send_invite_users()
+        elif value == "subscribe_test":
+            user = form_json['user']['username']
+            print('[Slack API] ' + user + ' has subscribed to the test.')
+            ts = form_json['container']['message_ts']
+            send_state_subscribed(ts)
+            # TODO: Ask for the ip. Ideas: Open modal, send private message, etc...
     return make_response("", 200)
 
 
 if __name__ == "__main__":
+    send_init()
     app.run(host='0.0.0.0')
