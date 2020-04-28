7fed7d9406e2a422dd3f91e69b8add57f11c4b5e
celiallanes <llanes.celia@icloud.com>
2020-04-28 13:11:43 +0200

    Notification (trap) no completa

--- snmp_api.py
+++ snmp_api.py
@@ -1,10 +1,12 @@
 from pysnmp.hlapi import *
+
+
 #
 # Functions to allow easily use GET and SET operations from SNMP in rest of code.
 #
 
 
-def set_scalar(ip,port, oid, value):
+def set_scalar(ip, port, oid, value):
     errorIndication, errorStatus, errorIndex, varBinds = next(
         setCmd(SnmpEngine(),
                CommunityData('public'),
@@ -72,3 +74,23 @@ def get_table_item(ip, port, column_oid, index, return_type):
     else:
         # Returning only one value of those found
         return str(varBinds[0].__getitem__(1)) if return_type == 1 else int(varBinds[0].__getitem__(1))
+
+
+
+    #No se como si indican los valores umbrales en el agente.
+    #No se muy bien que poner el return
+def send_trap(ip, port, oid):
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        sendNotification(SnmpEngine(),
+                         CommunityData('public'),
+                         UdpTransportTarget((ip, int(port))),
+                         ContextData(),
+                         'trap',
+                         NotificationType(ObjectIdentity(oid))
+                         )
+    )
+    if errorIndication or errorIndex or errorIndex:
+        print('[SNMP API] Error trap:' + oid)
+    else:
+        return "Trap"
+
