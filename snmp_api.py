28ac39da0f38b48fc5a3433dfbdd10408143fe23
jacjurtab <rojacin99@gmail.com>
2020-05-01 14:43:36 +0200

    Añadida funcion trap_engine que devuelve la trap_list

--- snmp_api.py
+++ snmp_api.py
@@ -1,4 +1,10 @@
 from pysnmp.hlapi import *
+from pysnmp.entity import engine, config
+from pysnmp.carrier.asyncore.dgram import udp
+from pysnmp.entity.rfc3413 import ntfrcv
+import asyncio
+
+trap_list = ['']
 
 
 #
@@ -76,20 +82,43 @@ def get_table_item(ip, port, column_oid, index, return_type):
         return str(varBinds[0].__getitem__(1)) if return_type == 1 else int(varBinds[0].__getitem__(1))
 
 
-# TODO: Darle oid(cambiar número por la string), y el valor a trapear como parámetros
 def send_trap(ip, port, oid, value):
-    errorIndication,errorStatus, errorIndex, varBinds=next(sendNotification(SnmpEngine(),
-                     CommunityData('public'),
-                     UdpTransportTarget((ip, int(port))),
-                     ContextData(),
-                     'trap',
-                     [ObjectType(ObjectIdentity(oid), Integer(value))]))
+    errorIndication, errorStatus, errorIndex, varBinds = next(sendNotification(SnmpEngine(),
+                                                                               CommunityData('public'),
+                                                                               UdpTransportTarget((ip, int(port))),
+                                                                               ContextData(),
+                                                                               'trap',
+                                                                               [ObjectType(ObjectIdentity(oid),
+                                                                                           Integer(value))]))
     if errorIndication or errorIndex or errorIndex:
         print('[SNMP API] Error sending trap for:' + oid)
     else:
         return True
 
 
+# function for receiving trap notifications
+# TODO probarlo
+def callback_trap(snmpEngine, stateReference, contextEngineId, contextName, varBinds, cbCtx):
+    sender_domain, sender_address = snmpEngine.msgAndPduDsp.getTransportInfo(stateReference)
+    # print de comprobacion
+    print('Notification from ' + sender_address)
+    cbCtx.append(sender_address)
+
+
+def trap_engine(ip, port):
+    loop = asyncio.get_event_loop()
+    snmpEngine = engine.SnmpEngine()
+    print('TrapServer listening on' + ip + ":" + port)
+    config.addTransport(
+        snmpEngine,
+        udp.domainName + (1,),
+        udp.UdpTransport().openServerMode((ip, port))
+    )
+    config.addV1System(snmpEngine, 'my-area', 'public')
+    global_list = ntfrcv.NotificationReceiver(snmpEngine, callback_trap, cbCtx=trap_list)
+    return global_list
+    loop.run_forever()
+
+
 if __name__ == "__main__":
     send_trap("192.168.1.111", 162, '1.3.6.1.2.1.4.3.0', 31060000)
-
