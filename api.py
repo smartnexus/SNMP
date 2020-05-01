f8fdf4c9cb0ede7c7915d8e61a37d43ec8c86ec3
smartnexus <angrodboh@alum.us.es>
2020-05-02 00:55:20 +0200

    Working on main app structure

--- api.py
+++ api.py
@@ -1,14 +1,23 @@
 from mibs import host_mib, rfc1213_mib, lanmgr_mib
 from snmp_api import *
-from main import port, port_trap, discard_sw
 from slack_api import *
 import uuid
 import statistics
 
 integer = 0
 string = 1
+
 trap_list = []
 users = []
+
+port = 161
+port_trap = 162
+ipInReceives_thr = 5000000
+ipInHdrErrors_thr = 0
+ipInAddrErrors_thr = 0
+discard_sw = ['update.exe', 'SkypeBackgroundHost.exe', 'SkypeBridge.exe', 'SkypeApp.exe',
+              'Skype.exe', 'MicrosoftEdge.exe', 'MicrosoftEdgeCP.exe', 'MicrosoftEdgesh.exe',
+              'OneDrive.exe', 'firefox.exe', 'java.exe']
 
 
 def add_agent(ip, slack_user):
@@ -16,7 +25,8 @@ def add_agent(ip, slack_user):
         user = {
             "id": uuid.uuid4().hex,
             "ip": ip,
-            "slack": slack_user
+            "slack": slack_user,
+            "discard": False
         }
         set_scalar(ip, port, rfc1213_mib.get('system').get('sysName'), user['id'])
         set_scalar(ip, port, rfc1213_mib.get('system').get('sysContact'), user['slack'])
@@ -25,11 +35,12 @@ def add_agent(ip, slack_user):
 
 def get_discard(ip):
     OS_index = get_scalar(ip, port, host_mib.get('hrSWRun').get('hrSWOSIndex'), string)
-    os_cpu_perf = get_table_item(ip, port, host_mib.get('hrSWRunPerf').get('hrSWRunPerfTable'), OS_index, integer)
+    os_cpu_perf = get_table_item(ip, port, host_mib.get('hrSWRunPerf').get('hrSWRunPerfCPU'), OS_index, integer)
     installed_sw = get_table_items(ip, port, host_mib.get('hrSWInstalled').get('hrSWInstalledName'), string)
     run_sw = get_table_items(ip, port, host_mib.get('hrSWRun').get('hrSWRunName'), string)
+    percent_os_cpu = round(os_cpu_perf / 24000000, 2)
 
-    return os_cpu_perf, installed_sw.__contains__('MATLAB'), any(elem in run_sw for elem in discard_sw)
+    return percent_os_cpu, any(installed.startswith('MATLAB') for installed in installed_sw), any(elem in run_sw for elem in discard_sw)
 
 
 def get_data(ip):
@@ -85,6 +96,6 @@ def trap_server_init():
 
 
 def trap_config(ip):
-    control = send_trap(ip, port_trap, rfc1213_mib.get('ip').get('ipInReceives'), ipInReceives_thr)
-    # TODO: llamar a send_trap() para resto las variables de las que quiera recibir traps
-    return control
+    send_trap(ip, port_trap, rfc1213_mib.get('ip').get('ipInReceives'), ipInReceives_thr)
+    send_trap(ip, port_trap, rfc1213_mib.get('ip').get('ipInHdrErrors'), ipInHdrErrors_thr)
+    send_trap(ip, port_trap, rfc1213_mib.get('ip').get('ipInAddrErrors'), ipInAddrErrors_thr)
--- main.py
+++ main.py
@@ -8,66 +8,85 @@ port = 161
 port_trap = 162
 test_time = 60  # duracion de la prueba
 sample_time = 5
-count = 0  # variable que controlará el tiempo de duracion de la prueba
-discard = False
 
 # UMBRALES
 cpu_thr = 2
 ram_thr = 4
 uptime_thr = 43200000
+os_max_processor_load = 0.05
 ipInReceives_thr = 5000000
+ipInHdrErrors_thr = 0
+ipInAddrErrors_thr = 0
 discard_sw = ['update.exe', 'SkypeBackgroundHost.exe', 'SkypeBridge.exe', 'SkypeApp.exe',
               'Skype.exe', 'MicrosoftEdge.exe', 'MicrosoftEdgeCP.exe', 'MicrosoftEdgesh.exe',
               'OneDrive.exe', 'firefox.exe', 'java.exe']
 
-
 # TODO: (1)Iniciar el slack y que me devuelva la lista de ips (list_ip) de todos los usuarios que realizan la prueba. (¿y la lisa de slack_users?)
 
-#init_slack()
+trap_server_init()
+add_agent('127.0.0.1', 'angel')
+add_agent('127.0.0.1', 'cecilia')
 
-for ip in users:  # la lista users ya contendrá todos las ips e informacion de usuario para la prueba
-    count = 0
+for user in users:
+    ip = user['ip']
+    domain = user['slack'] + '@' + user['ip']
+    user['discard'] = []
     discard = False
-    n_user = users.index(ip)  # TODO: Reformat necesario ya que users no es una lista de objetos simples.
-    trap_config(ip)       #Configuro traps
+    trap_config(ip)
 
-    print('[Main] Starting analysis for', ip + ':' + str(port))
+    print('[Main] Starting analysis for', domain)
 
-    print('[Main] Getting device information...')
+    print('[Main] Getting device information for ' + domain + '...')
+
     static_data = get_static_data(ip)
-    print(get_static_data(ip))
-    # TODO: Comprobar si los datos pueden suponer el descarte de la prueba
+
     if static_data['cpu_cores'] < cpu_thr:
         discard = True
-        # TODO: Añadir a arbol json, rama discard, la variable cpu_cores
-        # list[n_user].['discard'].append(static_data['cpu_cores'])
-        print('[DISCARD] cpu_cores < ', cpu_thr)
+        user['discard'].append({"cpu_cores": static_data['cpu_cores']})
+        print('[DISCARD] ' + domain + ' has cpu_cores < ', cpu_thr)
 
     if static_data['installed_ram'] < ram_thr:
         discard = True
-        # TODO: Añadir a arbol json, rama discard, la variable installed_ram
-        # list[n_user].['discard'].append(static_data['installed_ram'])
-        print('[DISCARD] installed_ram < ', ram_thr)
+        user['discard'].append({"installed_ram": static_data['installed_ram']})
+        print('[DISCARD] ' + domain + ' installed_ram < ', ram_thr)
 
     if static_data['uptime'] > uptime_thr:
         discard = True
-        # TODO: Añadir a arbol json, rama discard, la variable uptime
-        # list[n_user].['discard'].append(static_data['uptime'])
-        print('[DISCARD] uptime >', uptime_thr)
+        user['discard'].append({"uptime": static_data['uptime']})
+        print('[DISCARD] ' + domain + ' uptime >', uptime_thr)
 
-    # TODO: Comprobacion de processorLoads
+    os_processor_load, matlab, os_installed = get_discard(ip)
 
+    if os_processor_load > os_max_processor_load:
+        discard = True
+        user['discard'].append({"os_processor_load": os_processor_load})
+        print('[DISCARD] ' + domain + ' os_processor_load >', uptime_thr)
+    if not matlab:
+        discard = True
+        user['discard'].append({"matlab_installed": matlab})
+        print('[DISCARD] ' + domain + ' matlab not installed')
 
-    # TODO: añadir los datos estaticos al arbol json (en lo comentado he puesto arbol inventado de ejemplo)
-    # list[n_user].['date'].append(static_data['date'])
-    # list[n_user].['hardware'].append(static_data['hardware'])
-    # list[n_user].['os_version'].append(static_data['os_version'])
-    # list[n_user].['uptime'].append(static_data['uptime'])
-    # list[n_user].['cpu_cores'].append(static_data['cpu_cores'])
-    # list[n_user].['installed_ram'].append(static_data['installed_ram'])
+    if os_installed:
+        discard = True
+        user['discard'].append({"malware_installed": os_installed})
+        print('[DISCARD] ' + domain + ' malware installed')
 
-    print('[Main] Gathering device use each 5 secs...')
-    while count < test_time:
+    user['test_date'] = static_data['date']
+    user['hardware'] = static_data['hardware']
+    user['os_version'] = static_data['os_version']
+    user['uptime'] = static_data['uptime']
+    user['cpu_cores'] = static_data['cpu_cores']
+    user['installed_ram'] = static_data['installed_ram']
+
+    if discard:
+        print('[Main] Discarding test for user:', user['slack'])
+
+print('[Main] Gathering device use each ' + str(sample_time) + ' secs...')
+count = 0
+while count < test_time:
+    for user in users:
+        ip = user['ip']
+        domain = user['slack'] + '@' + user['ip']
         variable_data = get_variable_data(ip)
         print(get_variable_data(ip))
         # TODO: compobar si este usuario deberia ser descartado para la prueba
@@ -78,15 +97,7 @@ for ip in users:  # la lista users ya contendrá todos las ips e informacion de 
 
 
         # TODO: añadir los datos estaticos al arbol json (en lo comentado he puesto arbol inventado de ejemplo
-        # list[n_user].['used_cpu'].append(variable_data['used_cpu'])
-        # list[n_user].['used_ram'].append(variable_data['used_ram'])
-        count += sample_time
-        time.sleep(sample_time)
-
-# TODO: Comprobar si la prueba ha sido valida
-if not discard:
-    print('Prueba realizada correctamente')
-else:
-    print('Alguna variable no cumple los umbrales, descartamos prueba')
-
-# TODO: Enviar todos los datos recogidos (agol json) a slack con la función xxxAngelitorb99xxx.
+        # user['used_cpu'].append(variable_data['used_cpu'])
+        # user['used_ram'].append(variable_data['used_ram'])
+    count += sample_time
+    time.sleep(sample_time)
